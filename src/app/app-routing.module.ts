741eec1cf2026053b10591ede85a2f4e5fe237f6
Wen Ting Wang <chubberlisk@outlook.com>
2018-09-11 12:17:11 +0100

    Created a not-found component for 404s and redirect to page if invalid URL entered

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  RouterModule,
-  Routes
-} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { SillyComponent } from './silly/silly.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'silly', component: SillyComponent }
+  { path: 'silly', component: SillyComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
