24747db3ee6682b96ecfc1d774ed9ac008049315
Wen Ting Wang <chubberlisk@outlook.com>
2018-11-19 20:13:31 +0000

    Updated snackbar to say what should be expected when clicking change theme

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,14 +20,17 @@ export class HomeComponent implements OnInit {
   }
 
   changeTheme(): void {
+    var snackbarMsg = ""
     if ($("#change-theme-btn").hasClass("theme-multiple")) {
       // Yellow theme
       $(".landing, .a-bit-about-me, .education, .experience, .portfolio").css({"background-color": "var(--w-yellow)", "color": "black"});
       $("a").css("color", "var(--primary)");
-      $(".click-me a, #links a, #contact-me a").css("color", "white");
+      $(".click-me a, #links a, #contact-me a").css("color", "black");
+      $("#links a, #contact-me a").css("color", "white");
       $("#change-theme-btn").removeClass("theme-multiple");
       $("#change-theme-btn").addClass("theme-yellow");
       $("#change-theme-btn").html("Blue Theme <i class='fas fa-palette'></i>");
+      snackbarMsg = "All sections are yellow."
     } else if ($("#change-theme-btn").hasClass("theme-yellow")) {
       // Blue theme
       $(".landing, .a-bit-about-me, .education, .experience, .portfolio").css({"background-color": "#004385", "color": "white"});
@@ -36,6 +39,7 @@ export class HomeComponent implements OnInit {
       $("#change-theme-btn").removeClass("theme-yellow");
       $("#change-theme-btn").addClass("theme-blue");
       $("#change-theme-btn").html("Multiple Colours Theme <i class='fas fa-palette'></i>");
+      snackbarMsg = "All sections are blue."
     } else {
       // Multiple colours theme
       $(".landing").css("background-color", "var(--w-yellow)");
@@ -49,9 +53,10 @@ export class HomeComponent implements OnInit {
       $("#change-theme-btn").removeClass("theme-blue");
       $("#change-theme-btn").addClass("theme-multiple");
       $("#change-theme-btn").html("Yellow Theme <i class='fas fa-palette'></i>");
+      snackbarMsg = "All sections are different colours."
     }
     $("#snackbar").css("background-color", "var(--success)");
-    $("#snackbar").html("Theme changed!");
+    $("#snackbar").html("Theme changed! " + snackbarMsg);
     $("#snackbar").addClass("show");
     setTimeout(function(){ $("#snackbar").removeClass("show"); }, 3000);
   }
