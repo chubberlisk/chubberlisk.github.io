e8c972c0554f61be6737c14c092ccc6bed9fda67
Wen Ting Wang <chubberlisk@outlook.com>
2019-11-09 20:53:13 +0000

    Add basic Gatsby site

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <>
+    <SEO title="Home" />
+    <main>
+      Hello world.
+    </main>
+  </>
+)
+
+export default IndexPage
