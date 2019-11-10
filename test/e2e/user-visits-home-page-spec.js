bca4e83c15d63fefabab6ef26fe460d72cdca761
Wen Ting Wang <chubberlisk@outlook.com>
2019-11-10 17:29:08 +0000

    Refactor visits home page e2e test to be BDD style

--- test/e2e/user-visits-home-page-spec.js
+++ test/e2e/user-visits-home-page-spec.js
@@ -1,7 +1,15 @@
 describe("User visits home page", function() {
   it("shows the landing section", function() {
-    cy.visit("/")
+    whenIVisitTheHomePage()
 
-    cy.get(".landing").should("include.text", "Hi there!")
+    thenISeeTheWelcomeText()
   })
+
+  function whenIVisitTheHomePage() {
+    cy.visit("/")
+  }
+
+  function thenISeeTheWelcomeText() {
+    cy.get(".landing").should("include.text", "Hi there!")
+  }
 })
