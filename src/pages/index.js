4b314fa5cd6e6c03a1a012db66cb17cd101f268e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-10 00:12:00 +0000

    Deploy chubberlisk/chubberlisk.github.io to github.com/chubberlisk/chubberlisk.github.io.git:master

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import SEO from "../components/SEO/seo"
+import Landing from "../components/Landing/landing"
+
+const IndexPage = () => (
+  <>
+    <SEO title="Home" />
+    <Landing/>
+  </>
+)
+
+export default IndexPage
--- styles-bd9c9f342451fe5799a9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"+eM2":function(n,o,w){},PkBa:function(n,o,w){},"t+Ps":function(n,o,w){}}]);
-//# sourceMappingURL=styles-bd9c9f342451fe5799a9.js.map
