c86d929ce73e5a82420496ef8acd25a515ae05b6
Wen Ting Wang <chubberlisk@outlook.com>
2019-11-23 20:06:10 +0000

    Add footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from "react"
 
 import SEO from "../components/SEO/SEO"
 import Landing from "../components/Landing/Landing"
+import Footer from "../components/Footer/Footer"
 
 const IndexPage = () => (
   <>
     <SEO title="Home" />
     <Landing />
+    <Footer />
   </>
 )
 
